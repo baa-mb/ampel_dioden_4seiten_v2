5c256efd09280fb1787c7b0936b0478cd55f04d0
baa <34665120+baa-mb@users.noreply.github.com>
2020-05-24 20:58:38 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 function gelb (aNum: number) {
     pins.analogWritePin(ampelseite[aNum]+1, dioden_helligkeit)
+aNum2 = (aNum + 1) % 2
+    pins.analogWritePin(ampelseite[aNum2]+1, dioden_helligkeit)
 basic.pause(warte_kurz)
+    alle_aus(aNum)
+    alle_aus(aNum2)
 }
 function gruen (aNum: number) {
     pins.analogWritePin(ampelseite[aNum]+0, 0)
@@ -9,12 +13,11 @@ pins.analogWritePin(ampelseite[aNum]+2, dioden_helligkeit)
 basic.pause(warte_lang)
     for (let index = 0; index < 4; index++) {
         pins.analogWritePin(ampelseite[aNum]+2, 0)
-basic.pause(warte_blink / 2)
+basic.pause(warte_blink)
         pins.analogWritePin(ampelseite[aNum]+2, dioden_helligkeit)
 basic.pause(warte_blink)
     }
     alle_aus(aNum)
-    gelb(aNum)
 }
 function rot (aNum: number) {
     alle_aus(aNum)
@@ -30,15 +33,16 @@ let warte_blink = 0
 let warte_kurz = 0
 let warte_lang = 0
 let dioden_helligkeit = 0
+let aNum2 = 0
 basic.showIcon(IconNames.Heart)
 let ampelseite = [100, 112]
 dioden_helligkeit = 512
 let zfakt = 2
 warte_lang = 5000 * zfakt
 warte_kurz = 2000 * zfakt
-warte_blink = 200 * zfakt
+warte_blink = 300 * zfakt
 basic.forever(function () {
     rot(0)
     gelb(0)
-    gruen(0)
+    rot(1)
 })
